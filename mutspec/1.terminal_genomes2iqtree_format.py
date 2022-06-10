4b3b7c9de547ef606d2acec102a659229fd439e9
kpotoh <axepon@mail.ru>
2022-06-10 18:26:55 +0200

    1st sctipt update & almost ready to recalc prob ms

--- mutspec/1.terminal_genomes2iqtree_format.py
+++ mutspec/1.terminal_genomes2iqtree_format.py
@@ -7,80 +7,76 @@ Internal States must be rewritten to similar format
 
 import os
 import sys
+import glob
 from collections import defaultdict
 from typing import Tuple
 
 import click
-import tqdm
 from Bio import SeqIO
 
-from mutspec.utils import load_scheme, get_aln_files
 
-NGENES = 12
+def get_aln_files(path: str):
+    assert os.path.isdir(path), "path is not directory"
+    files = set(glob.glob(os.path.join(path, "*.fna")))
+    return files
 
 
-def parse_alignment(files: list, scheme: dict, aln_dir, out) -> Tuple[str, int]:
+def parse_alignment_and_write_states(files: list, outfile) -> Tuple[str, int]:
     """
     read fasta files from scheme with alignments and write states to table
 
     return states table and full alignment length
     """
-    handle = open(out, "w")
+    print(f"Processing...", file=sys.stderr)
+    handle = open(outfile, "w")
+    ngenes = len(files)
     columns = "Node Part Site State p_A p_C p_G p_T".split()
     handle.write("\t".join(columns) + "\n")
-    aln_lens = []
+    aln_lens = dict()
     files = set(files)
     history = defaultdict(list)
-    for part, gene_fn in tqdm.tqdm(scheme.items(), "Parts"):
-        filepath = os.path.join(aln_dir, gene_fn)
-        assert filepath in files, f"cannot find file {filepath} from scheme"
+    for filepath in files:
+        gene = os.path.basename(filepath).replace(".fna", "")
         fasta = SeqIO.parse(filepath, "fasta")
         for rec in fasta:
             node = rec.name
-            history[node].append(part)
+            history[node].append(gene)
             seq = str(rec.seq)
             for site, state in enumerate(seq, 1):
-                pos_data = [node, str(part), str(site), state]
+                pos_data = [node, str(gene), str(site), state]
                 for nucl in "ACGT":
                     p = int(nucl == state)
                     pos_data.append(str(p))
 
                 handle.write("\t".join(pos_data) + "\n")
-        aln_lens.append(len(seq))
+        aln_lens[gene] = len(seq)
 
     # get max set of parts
     for node, parts in history.items():
-        if len(parts) == NGENES:
+        if len(parts) == ngenes:
             full_parts = parts.copy()
             break
-    
+
     # fill missing genes by '-'
     for node, parts in history.items():
-        if len(parts) != NGENES:
+        if len(parts) != ngenes:
             unseen_parts = set(full_parts).difference(parts)
             for unp in unseen_parts:
                 print(f"Gap filling for node {node}, part {unp}...", file=sys.stderr)
-                for site in range(1, aln_lens[unp - 1] + 1):
-                    pos_data = [node, str(unp), str(site), "-", "0", "0", "0", "0"]
+                for site in range(1, aln_lens[unp] + 1):
+                    pos_data = [node, unp, str(site), "-", "0", "0", "0", "0"]
                     handle.write("\t".join(pos_data) + "\n")
-
     handle.close()
-    full_aln_len = sum(aln_lens)
-    return full_aln_len
 
 
 @click.command("formatter", help="reformat alignment to states table")
-@click.option("--aln", "aln_dir", required=True, type=click.Path(True), help="path to directory with gene alignment files")
-@click.option("--scheme", "scheme_path", required=True, type=click.Path(True), help="path to scheme that contain gene splitting info of alignment")
+@click.option("--aln", required=True, type=click.Path(True), help="path to directory with gene alignment files")
 @click.option("--out", required=True, type=click.Path(writable=True), help="path to output states file (tsv)")
-def main(aln_dir, scheme_path, out):
-    aln_files = get_aln_files(aln_dir)
-    scheme = load_scheme(scheme_path)
-    assert len(scheme) > 0
-    print(scheme)
-    aln_len = parse_alignment(aln_files, scheme, aln_dir, out)
+def main(aln, out):
+    aln_files = get_aln_files(aln)
+    parse_alignment_and_write_states(aln_files, out)
 
 
 if __name__ == "__main__":
     main()
-    # main("data/interim/alignments_birds_clean_clean", "data/interim/scheme_birds_genes.nex", "data/interim/leaves_birds_states.tsv")
+    # main("./data/example_nematoda/alignments_nematoda_clean/", "/tmp/leaves_birds_states.tsv")
