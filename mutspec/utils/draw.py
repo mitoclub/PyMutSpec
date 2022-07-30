53698889670a37636cc96ddc68f5fcf333965183
kpotoh <axepon@mail.ru>
2022-07-30 13:26:38 +0200

    plot_mutspec: copied style from COSMIC

--- mutspec/utils/draw.py
+++ mutspec/utils/draw.py
@@ -7,10 +7,45 @@ import seaborn as sns
 from .constants import possible_sbs12, possible_sbs192
 
 
+coloring6 = {
+    "C>A": "deepskyblue",
+    "C>G": "black",
+    "C>T": "red",
+    "T>A": "silver",
+    "T>C": "green",
+    "T>G": "pink",
+}
+coloring12 = {
+    "C>A": "deepskyblue",
+    "G>T": "deepskyblue",
+    "C>G": "black",
+    "G>C": "black",
+    "C>T": "red",
+    "G>A": "salmon",
+    "T>A": "silver",
+    "A>T": "silver",
+    "T>C": "green",
+    "A>G": "green",
+    "T>G": "pink",
+    "A>C": "pink",
+}
+_smpl = pd.DataFrame({"Mut": possible_sbs192})
+_smpl["MutBase"] = _smpl.Mut.str.slice(2, 5)
+_smpl["Context"] = _smpl.Mut.str.get(0) + _smpl.Mut.str.get(2) + _smpl.Mut.str.get(-1)
+colors192 = _smpl.sort_values(["MutBase", "Context"])["MutBase"].map(coloring12).values
+colors12 = [coloring12[sbs] for sbs in possible_sbs12]
+
 def plot_mutspec12(mutspec: pd.DataFrame, ylabel="MutSpec", title="Full mutational spectra", savepath=None):
+    # TODO add checks of mutspec12
+    # TODO add description to all plot* functions
     fig = plt.figure(figsize=(6, 4))
     ax = fig.add_subplot(111)
     ax = sns.barplot(x="Mut", y=ylabel, data=mutspec, order=possible_sbs12, ax=fig.gca())
+
+    # map colors to bars
+    for bar, clr in zip(ax.patches, colors12):
+        bar.set_color(clr)
+
     ax.set_title(title)
     if savepath is not None:
         plt.savefig(savepath)
@@ -69,6 +104,7 @@ def plot_mutspec192(mutspec192: pd.DataFrame, ylabel="MutSpec", title="Mutationa
     filepath: str, default = None
         Path to output plot file. If None no images will be written
     """
+    # TODO add checks of mutspec192
     mutspec192 = mutspec192.copy()
     mutspec192["MutBase"] = mutspec192.Mut.str.slice(2, 5)
     mutspec192["Context"] = mutspec192.Mut.str.get(0) + mutspec192.Mut.str.get(2) + mutspec192.Mut.str.get(-1)
@@ -80,6 +116,11 @@ def plot_mutspec192(mutspec192: pd.DataFrame, ylabel="MutSpec", title="Mutationa
         x="Mut", y=ylabel, data=mutspec192,
         order=possible_sbs192, errwidth=1, ax=fig.gca()
     )
+    # map colors to bars
+    for bar, clr in zip(ax.patches, colors192):
+        bar.set_color(clr)
+        bar.set_width(0.5)
+    
     labels = ['' for _ in ax.get_xticklabels()]
     ax.set_xticklabels(labels)
     ax.set_xlabel('')
