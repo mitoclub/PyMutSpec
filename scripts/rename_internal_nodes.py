33e23a32ce0c7d3cc1063a9637b539a9e6c27d2a
kpotoh <axepon@mail.ru>
2022-09-29 18:58:37 +0200

    rename continues

--- scripts/rename_internal_nodes.py
+++ scripts/rename_internal_nodes.py
@@ -1,27 +1,14 @@
-"""
-script add names for internal nodes in newick file 
-"""
 import sys
-from ete3 import PhyloTree
-
-dist_formatter="%0.8f"
 
 
-def main(path_to_tree, path_to_out):
-    tree = PhyloTree(path_to_tree, format=1)
-
-    i = 1
-    for node in tree.traverse():
-        if not node.name:
-            node.name = "Node{}".format(i)
-            i += 1
-
-    tree.write(format=1, outfile=path_to_out, dist_formatter=dist_formatter)
+def main(path_to_dist_tree, path_to_named_tree, path_to_out):
+    # Iteratively going from leaves to root and add names to dist tree
+    pass
 
 
 if __name__ == "__main__":
     try:
-        path_to_tree, path_to_out = sys.argv[1:]
-        main(path_to_tree, path_to_out)
+        path_to_dist_tree, path_to_named_tree, path_to_out = sys.argv[1:]
+        main(path_to_dist_tree, path_to_named_tree, path_to_out)
     except:
-        print("ERROR\nUSAGE: script.py path_to_tree path_to_out_tree", file=sys.stderr)
+        print("ERROR\nUSAGE: script.py path_to_dist_tree path_to_named_tree path_to_out_tree", file=sys.stderr)
