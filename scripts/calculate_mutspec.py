5fe27d860309f513dc9aedb9e0911285a7068306
kpotoh <axepon@mail.ru>
2022-10-11 18:00:20 +0200

    c

--- scripts/calculate_mutspec.py
+++ scripts/calculate_mutspec.py
@@ -1,37 +1,35 @@
-import click
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import os
 
-from mutspec_utils.draw import plot_mutspec192kk, plot_mutspec192, plot_mutspec12
-from mutspec_utils.constants import possible_sbs12, possible_sbs192
+import click
+import pandas as pd
+
+from mutspec_utils.draw import plot_mutspec192, plot_mutspec12
 from mutspec_utils.annotation import calculate_mutspec
 
 OUTGRP = "OUTGRP"
-LABELS = [0, 1, 2]
 MUT_NUM_FOR_192 = 150
+LABELS = [0, 1, 2]
 
-# plot_mutspec192kk(df, )
-# plot_mutspec192(df)
 
-# @click.command("MutSpec calculator", help="TODO")
-# @click.option("--db_path", "path_to_db", type=click.Path(writable=True), default="/tmp/states.db", show_default=True, help="Path to database with states. Use only with --write_db")
-# @click.option("--rewrite_db",  is_flag=True, default=False, help="Rewrite existing states database. Use only with --write_db")
-# @click.option('-f', '--force', is_flag=True, help="Rewrite existing output directory")
-# @click.option('-v', '--verbose', "verbosity", count=True, help="Verbosity level = DEBUG")
-def main():
-    path_to_obs = "./tmp/mutations.tsv"
-    path_to_exp = "./tmp/expected_mutations.tsv"
-    path_to_ms12 = "./tmp/ms12{}.tsv"
-    path_to_ms12plot = "./tmp/ms12{}.png"
-    path_to_ms192plot = "./tmp/ms192{}.png"
-    path_to_ms192 = "./tmp/ms192{}.tsv"
+@click.command("MutSpec calculator", help="Calculate and visualize mutational spectra")
+@click.option("-b", "--observed", "path_to_obs", type=click.Path(True),  show_default=True, help="Path to observed mutations table")
+@click.option("-e", "--expected", "path_to_exp", type=click.Path(True), help="Path to expected mutations table")
+@click.option("-o", '--outdir', type=click.Path(True), default=".", show_default=True, help="Path to output directory for files (must exist)")
+@click.option('--outgrp', default=OUTGRP, show_default=True, help="Name of outgroup node in the tree to exclude from mutations set")
+@click.option('--mnum192', default=MUT_NUM_FOR_192, show_default=True, help="Number of mutations required to calculate and plot 192-component mutational spectra")
+def main(path_to_obs, path_to_exp, outdir, outgrp, mut_num_for_192):
+    path_to_united_exp = os.path.join(outdir, "mean_expexted_mutations.tsv")
+    path_to_ms12 = os.path.join(outdir, "ms12{}.tsv")
+    path_to_ms12plot = os.path.join(outdir, "ms12{}.png")
+    path_to_ms192plot = os.path.join(outdir, "ms192{}.png")
+    path_to_ms192 = os.path.join(outdir, "ms192{}.tsv")
 
     obs = pd.read_csv(path_to_obs, sep="\t")
     exp_raw = pd.read_csv(path_to_exp, sep="\t")
 
-    obs = obs[obs.AltNode != OUTGRP]
+    obs = obs[obs.AltNode != outgrp]
     exp = exp_raw.drop_duplicates().drop(["Node", "Gene"], axis=1).groupby("Label").mean()
+    exp.reset_index().to_csv(path_to_united_exp, sep="\t", index=None)
 
     for lbl_code in LABELS:
         if lbl_code == 0:
@@ -42,15 +40,16 @@ def main():
             lbl = "ff"
 
         cur_obs = obs[obs.Label >= lbl_code]
-        cur_exp = exp.loc[lbl].to_dict()
-        
-        ms12 = calculate_mutspec(cur_obs, cur_exp, use_context=False, use_proba=True)
-        ms12.to_csv(path_to_ms12.format(lbl), sep="\t", index=None)
-        plot_mutspec12(ms12, savepath=path_to_ms12plot.format(lbl), show=False)
-        if cur_obs.shape[0] > MUT_NUM_FOR_192:
-            ms192 = calculate_mutspec(cur_obs, cur_exp, use_context=True, use_proba=True)
-            ms192.to_csv(path_to_ms192.format(lbl), sep="\t", index=None)
-            plot_mutspec192(ms192, filepath=path_to_ms192plot.format(lbl))
+        if cur_obs.shape[0]:
+            cur_exp = exp.loc[lbl].to_dict()
+
+            ms12 = calculate_mutspec(cur_obs, cur_exp, use_context=False, use_proba=True)
+            ms12.to_csv(path_to_ms12.format(lbl), sep="\t", index=None)
+            plot_mutspec12(ms12, savepath=path_to_ms12plot.format(lbl), show=False)
+            if cur_obs.shape[0] > mut_num_for_192:
+                ms192 = calculate_mutspec(cur_obs, cur_exp, use_context=True, use_proba=True)
+                ms192.to_csv(path_to_ms192.format(lbl), sep="\t", index=None)
+                plot_mutspec192(ms192, filepath=path_to_ms192plot.format(lbl))
 
 
 if __name__ == "__main__":
