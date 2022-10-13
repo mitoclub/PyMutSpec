b0e247dbae1ac37c8962a39daa6587d593f886c6
kpotoh <axepon@mail.ru>
2022-10-13 11:47:34 +0200

    label to calc_ms script

--- scripts/calculate_mutspec.py
+++ scripts/calculate_mutspec.py
@@ -144,22 +144,28 @@ def plot_mutspec192(mutspec192: pd.DataFrame, ylabel="MutSpec", title="Mutationa
 @click.option("-b", "--observed", "path_to_obs", type=click.Path(True),  show_default=True, help="Path to observed mutations table")
 @click.option("-e", "--expected", "path_to_exp", type=click.Path(True), help="Path to expected mutations table")
 @click.option("-o", '--outdir', type=click.Path(True), default=".", show_default=True, help="Path to output directory for files (must exist)")
+@click.option("-l", '--label', default="", show_default=True, help="Label for files naming")
 @click.option('--outgrp', default=OUTGRP, show_default=True, help="Name of outgroup node in the tree to exclude from mutations set")
 @click.option('--mnum192', "mut_num_for_192", default=MUT_NUM_FOR_192, show_default=True, help="Number of mutations required to calculate and plot 192-component mutational spectra")
 @click.option('--ext', "image_extension", default="pdf", show_default=True, type=click.Choice(['pdf', 'png', 'jpg'], case_sensitive=False), help="Images format to save")
-def main(path_to_obs, path_to_exp, outdir, outgrp, mut_num_for_192, image_extension):
-    path_to_united_exp = os.path.join(outdir, "mean_expexted_mutations.tsv")
-    path_to_ms12 = os.path.join(outdir, "ms12{}.tsv")
-    path_to_ms12plot = os.path.join(outdir, "ms12{}." + image_extension)
-    path_to_ms192plot = os.path.join(outdir, "ms192{}.") + image_extension
-    path_to_ms192 = os.path.join(outdir, "ms192{}.tsv")
+def main(path_to_obs, path_to_exp, outdir, label, outgrp, mut_num_for_192, image_extension):
+    path_to_united_exp = os.path.join(outdir, "mean_expexted_mutations_{}.tsv".format(label))
+    path_to_ms12 = os.path.join(outdir, "ms12{}_{}.tsv")
+    path_to_ms12plot = os.path.join(outdir, "ms12{}_{}.{}")
+    path_to_ms192plot = os.path.join(outdir, "ms192{}_{}.{}")
+    path_to_ms192 = os.path.join(outdir, "ms192{}_{}.tsv")
 
     obs = pd.read_csv(path_to_obs, sep="\t")
     exp_raw = pd.read_csv(path_to_exp, sep="\t")
 
     obs = obs[obs.AltNode != outgrp]
     exp = exp_raw.drop_duplicates().drop(["Node", "Gene"], axis=1).groupby("Label").mean()
-    exp.reset_index().to_csv(path_to_united_exp, sep="\t", index=None)
+    # exp.reset_index().to_csv(path_to_united_exp, sep="\t", index=None)
+    exp_melted = exp.reset_index()
+    exp_melted["Label"] = exp_melted["Label"].where(exp_melted["Label"] != "ff", "syn4f")
+    exp_melted.melt("Label", exp_melted.columns.values[1:], var_name="Mut", value_name="MutSpec")\
+        .sort_values(["Mut", "Label"])\
+            .to_csv(path_to_united_exp, sep="\t", index=None)
 
     for lbl_code in LABELS:
         if lbl_code == 0:
@@ -174,14 +180,14 @@ def main(path_to_obs, path_to_exp, outdir, outgrp, mut_num_for_192, image_extens
             cur_exp = exp.loc[lbl].to_dict()
 
             ms12 = calculate_mutspec(cur_obs, cur_exp, use_context=False, use_proba=True)
-            ms12.to_csv(path_to_ms12.format(lbl), sep="\t", index=None)
+            ms12.to_csv(path_to_ms12.format(lbl, label), sep="\t", index=None)
             plot_mutspec12(ms12, title=f"{lbl} mutational spectrum",
-                           savepath=path_to_ms12plot.format(lbl), show=False)
+                           savepath=path_to_ms12plot.format(lbl, label, image_extension), show=False)
             if cur_obs.shape[0] > mut_num_for_192:
                 ms192 = calculate_mutspec(cur_obs, cur_exp, use_context=True, use_proba=True)
-                ms192.to_csv(path_to_ms192.format(lbl), sep="\t", index=None)
+                ms192.to_csv(path_to_ms192.format(lbl, label), sep="\t", index=None)
                 plot_mutspec192(ms192, title=f"{lbl} mutational spectrum",
-                                filepath=path_to_ms192plot.format(lbl), show=False)
+                                filepath=path_to_ms192plot.format(lbl, label, image_extension), show=False)
 
 
 if __name__ == "__main__":
