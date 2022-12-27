0ca3754285b2a70bff34f14adede8b63e1f14383
kpotoh <axepon@mail.ru>
2022-12-27 18:26:32 +0200

    fix script extraction

--- scripts/extract_genomes_from_gb.py
+++ scripts/extract_genomes_from_gb.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import random
 import sys
 
@@ -5,9 +6,11 @@ from Bio import SeqIO
 import tqdm
 
 N = 300
-p = 0.1
+p = 1
 print(N, p)
 
+# genes = ["CYTB", "ND1"]
+genes = []
 
 def main():
     try:
@@ -17,25 +20,29 @@ def main():
         print("USAGE: script.py GB_FILE OUT_FASTA")
 
     records = SeqIO.parse(path_in, format="gb")
-    nd1_seqs = []
-    cytb_seqs = []
+    seqs = defaultdict(list)
+
     for rec in tqdm.tqdm(records):
         if random.random() < p:
             for fea in rec.features:
                 if fea.type == "CDS":
-                    if fea.qualifiers["gene"][0] == "CYTB":
-                        cytb_seqs.append(fea.extract(rec))
-                    if fea.qualifiers["gene"][0] == "ND1":
-                        nd1_seqs.append(fea.extract(rec))
+                    for g in genes:
+                        if fea.qualifiers["gene"][0] == g:
+                            seqs[g].append(fea.extract(rec))
+                elif fea.type == "D-loop":
+                    new_rec = fea.extract(rec)
+                    new_rec.id = rec.id
+                    new_rec.name = rec.description
+                    new_rec.description = rec.description
+                    seqs["D-loop"].append(new_rec)
 
-        if len(nd1_seqs) == N:
+        if len(genes) and len(seqs[genes[0]]) == N:
             break
     
-    with open(path_out.replace(".fasta", "_cytb.fasta"), "w") as fout:
-        SeqIO.write(cytb_seqs, fout, "fasta")
-    
-    with open(path_out.replace(".fasta", "_nd1.fasta"), "w") as fout:
-        SeqIO.write(nd1_seqs, fout, "fasta")
+    for g in genes + ["D-loop"]:
+        with open(path_out.replace(".fasta", f"_{g}.fasta"), "w") as fout:
+            SeqIO.write(seqs[g], fout, "fasta")
+
 
 
 if __name__ == "__main__":
