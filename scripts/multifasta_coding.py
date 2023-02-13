11f6202188c4f5f73f3bfe14c9049392ac335794
kpotoh <axepon@mail.ru>
2023-02-13 20:32:05 +0300

    new script

---
+++ scripts/multifasta_coding.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import click
+from Bio import SeqIO
+
+
+def is_coded(aln):
+    ncoded = noutgrp = 0
+    for rec in aln:
+        if rec.description == "OUTGRP":
+            noutgrp += 1
+        elif rec.description.startswith("RN_"):
+            ncoded += 1
+    return ncoded == len(aln) - 1 and noutgrp == 1
+
+
+@click.command("Fasta header coder", help="")
+@click.option("-a", "--alignment", type=click.Path(True), help="Fasta nucleotide alignment")
+@click.option("-g", "--outgroup", help="Outrgoup header name")
+@click.option("-o", "--outfile", type=click.Path(writable=True), help="Output fasta file with coded headers")
+@click.option("-m", "--outmap", type=click.Path(writable=True), help="Mapping of headers to coders (txt)")
+def main(alignment, outgroup, outfile, outmap):
+    aln = [r for r in SeqIO.parse(alignment, "fasta")]
+
+    if is_coded(aln):
+        print("Coding is not required, headers already coded")
+    else:
+        sp_map = dict()
+        for i, rec in enumerate(aln, 1):
+            header = rec.description
+            coded_header = "OUTGRP" if header == outgroup else "RN_{}".format(i)
+            sp_map[coded_header] = header
+
+            rec.id = coded_header
+            rec.name = coded_header
+            rec.description = coded_header
+
+        with open(outmap, "w") as fout:
+            fout.write("\n".join(["{}\t{}".format(k, v) for k,v in sp_map.items()]))
+
+    SeqIO.write(aln, outfile, "fasta-2line")
+
+if __name__ == "__main__":
+    main()
