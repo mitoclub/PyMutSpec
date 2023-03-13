6460804452972a1a634cdf07dbea424eb45c8b78
kpotoh <axepon@mail.ru>
2023-03-13 18:50:13 +0200

    plot_mutspec96

--- pymutspec/draw/__init__.py
+++ pymutspec/draw/__init__.py
@@ -1,4 +1,4 @@
-from .spectra import plot_mutspec12, plot_mutspec192, plot_mutspec192kk, _prepare_nice_labels
+from .spectra import plot_mutspec12, plot_mutspec192, plot_mutspec192kk, _prepare_nice_labels, plot_mutspec96
 
 def plot_mutspec192box(*args, **kwargs):
     print("WARNING: this alias function is removed! Use plot_mutspec192(style='box') instead")
--- pymutspec/draw/spectra.py
+++ pymutspec/draw/spectra.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-from pymutspec.constants import possible_sbs192
+from pymutspec.constants import possible_sbs192, possible_sbs96
 from .sbs_orders import ordered_sbs192_kk, ordered_sbs192_kp
 
 color_mapping6 = {
@@ -155,6 +155,88 @@ def plot_mutspec192(
         if len(sbs):
             s = sbs[0] + ">" + sbs[1] if labels_style == "long" else sbs[2:5]
             bar.set_color(color_mapping12[s])
+            bar.set_alpha(alpha=0.9)
+        if style == "bar":
+            if not shift:
+                # calculate one time instead of 192
+                shift = (bar.get_width() - width) / 2
+            bar.set_width(width)
+            bar.set_x(bar.get_x() + shift)
+
+    plt.xticks(rotation=90, fontsize=fontsize, fontname=fontname)
+
+    if savepath is not None:
+        plt.savefig(savepath, dpi=300, bbox_inches="tight")
+    if show:
+        plt.show()
+    else:
+        plt.close()
+
+
+def plot_mutspec96(
+        spectra: pd.DataFrame, 
+        ylabel="MutSpec", 
+        title="Mutational spectrum", 
+        figsize=(15, 5), 
+        style="bar",
+        labels_style="cosmic",
+        sbs_order=possible_sbs96,
+        savepath=None,
+        fontsize=9,
+        fontname="Times New Roman",
+        show=True, 
+        **kwargs,
+    ):
+    """
+    Plot barblot of given mutational spectrum calculated from single nucleotide substitutions
+
+    Arguments
+    ---------
+    spectra: pd.DataFrame
+        table, containing 192 component mutational spectrum for one or many species, all substitutions must be presented in the table
+    title: str, default = 'Mutational spectrum'
+        Title on the plot
+    savepath: str, default = None
+        Path to output plot file. If None no images will be written
+    labels_style: str, default = 'cosmic'
+        'cosmic': A[C>T]T, 'long': CT: ACT
+    """
+    if "filepath" in kwargs:
+        savepath = kwargs["filepath"]
+        print("savepath =", savepath)
+        kwargs.pop("filepath")
+    
+    # TODO add checks of spectra
+    ms96 = spectra.copy()
+    if labels_style == "long":
+        ms96["long_lbl"] = ms96.Mut.str.get(2) + ms96.Mut.str.get(4) + ": " + ms96.Mut.str.get(0) + ms96.Mut.str.get(2) + ms96.Mut.str.get(-1)
+        order = _prepare_nice_labels(sbs_order, kk=True)
+        x_col = "long_lbl"
+    elif labels_style == "cosmic":
+        order = _prepare_nice_labels(sbs_order, kk=False)
+        x_col = "Mut"
+    else:
+        raise ValueError("Available labels_style are: 'cosmic' and 'long'")
+    fig = plt.figure(figsize=figsize)
+    if style == "bar":
+        ax = sns.barplot(
+            x=x_col, y=ylabel, data=ms96, order=order, errwidth=1, ax=fig.gca(), **kwargs,
+        )
+    elif style == "box":
+        ax = sns.boxplot(
+            x=x_col, y=ylabel, data=ms96, order=order, ax=fig.gca(), **kwargs,
+        )
+    ax.grid(axis="y", alpha=.7, linewidth=0.5)
+    ax.set_title(title)
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    # map colors to bars
+    width = 0.4
+    shift = None
+    for bar, sbs in zip(ax.patches, order):
+        if len(sbs):
+            s = sbs[0] + ">" + sbs[1] if labels_style == "long" else sbs[2:5]
+            bar.set_color(color_mapping6[s])
             bar.set_alpha(alpha=0.9)
         if style == "bar":
             if not shift:
