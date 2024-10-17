1768f06f4c5b371407793599120ec22d66ea5f02
kpotoh <axepon@mail.ru>
2024-10-17 16:13:33 +0200

    update version

--- scripts/calculate_mutspec.py
+++ scripts/calculate_mutspec.py
@@ -202,6 +202,4 @@ def main(
 
 if __name__ == "__main__":
     main()
-    # pt = '/home/kpotoh/nemu-pipeline/example_mam_run/outdir/mutspec_tables'
-    # main(f"-b {pt}/observed_mutations_iqtree.tsv -e {pt}/expected_freqs.tsv -o {pt}/test_branches -l debug --branches --syn".split())
     # main("-b data/mouse_cytb/tables/observed_mutations.tsv -e data/mouse_cytb/tables/expected_freqs.tsv -o data/mouse_cytb/test/ --syn --all -p --plot".split())
