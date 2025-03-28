0da4ecfdf3b968ff229da78dc7ed48410b4aa90e
kpotoh <axepon@mail.ru>
2025-03-28 14:37:38 +0200

    tests repaired

--- pymutspec/io/__init__.py
+++ pymutspec/io/__init__.py
@@ -1,9 +1,3 @@
 from .auxiliary import get_aln_files, load_scheme
 from .gb import read_genbank_ref
-from .states import GenomeStates, GenomeStatesTotal, read_rates, read_alignment
-
-__all__ = [
-    'GenomeStates', 'GenomeStatesTotal', 
-    'read_alignment', 'read_genbank_ref', 'read_rates', 
-    'get_aln_files', 'load_scheme',
-]
+from .states import GenesStates, GenomeStates, GenomeStatesTotal, read_rates, read_alignment
